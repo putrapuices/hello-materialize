f805108d6458df992374a533af024a9ac8fc9a07
putrapuices <puices14@gmail.com>
2021-12-05 21:46:24 +0700

    awalnya ya

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Button } from "react-materialize";
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Selamat Datang React Materialize</h1>
+        <Button>Sukses</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
