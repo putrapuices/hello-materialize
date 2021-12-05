f67855435291e6ffbb6c20c41cabcf82784ebaee
putrapuices <puices14@gmail.com>
2021-12-05 22:03:17 +0700

    Materialize - Card ( 103 )

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Card, CardTitle, Row, Col } from "react-materialize";
 import { Button } from "react-materialize";
 class App extends Component {
   render() {
@@ -6,6 +7,37 @@ class App extends Component {
       <div>
         <h1>Selamat Datang React Materialize</h1>
         <Button>Sukses</Button>
+        <Row>
+          <Col m="5">
+            {" "}
+            {/* //attribut m digunkana untuk ukuran layar */}
+            <Card
+              className="small"
+              header={
+                <CardTitle image="https://www.incimages.com/uploaded_files/image/970x450/products_364475.jpg">
+                  Taman Bermain
+                </CardTitle>
+              }
+            >
+              baju
+            </Card>
+          </Col>
+
+          <Col m="5">
+            {" "}
+            {/* //attribut m digunkana untuk ukuran layar */}
+            <Card
+              className="small"
+              header={
+                <CardTitle image="https://www.incimages.com/uploaded_files/image/970x450/products_364475.jpg">
+                  Taman Bermain
+                </CardTitle>
+              }
+            >
+              baju
+            </Card>
+          </Col>
+        </Row>
       </div>
     );
   }
